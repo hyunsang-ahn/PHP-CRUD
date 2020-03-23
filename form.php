7285df7660a06758814f02f2655f50c8b28a7d2c
ReturnSweetPork <happylovetkd@naver.com>
2020-03-24 08:25:32 +0900

    get방식과 post방식을 통한 form 생성

--- form.php
+++ form.php
@@ -1,3 +1,5 @@
 <?php
 file_put_contents('data/'.$_GET['title'], $_GET['description'])
+//method=get 이면 get으로 파라미터를 받아야함.
+//method=post 이면 post로 파라미터를 받아야함
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,45 @@
+<?php
+function print_title(){
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }else{
+        echo 'Welcome';
+    }    
+}
+
+function print_description(){
+    if(isset($_GET['id'])){
+
+        echo file_get_contents("data/".$_GET['id']);
+    }else{
+        echo 'Hello PHP';
+    }
+}
+function print_list(){
+    $list = scandir('./data');
+    $i = 0;
+    while($i < count($list)){
+        if($list[$i] != '.'){
+            if($list[$i] != '..'){
+                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+        }
+        $i = $i + 1;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        <?php
+            print_title()
+        ?> 
+    
+    </title>
 </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
@@ -13,38 +49,18 @@
         <li><a href="index.php?id=JAVASCRIPT">JAVASCRIPT</a></li>
         <li><a href="index.php?id=PHP">PHP</a></li> -->
         <?php
-         $list = scandir('./data');
-        $i = 0;
-        while($i < count($list)){
-            if($list[$i] != '.'){
-                if($list[$i] != '..'){
-
-                    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-                }
-
-            }
-            $i = $i + 1;
-        }
+            print_list()
         ?>
     </ol>
 
     <h2>
-
-    <?php 
-    if(isset($_GET['id'])){
-        echo $_GET['id'];
-    }else{
-        echo 'Welcome';
-    }
-    ?>
+        <?php
+        print_title()
+        ?> 
     </h2>
-    <?php
-    if(isset($_GET['id'])){
 
-        echo file_get_contents("data/".$_GET['id']);
-    }else{
-        echo 'Hello PHP';
-    }
+    <?php
+        print_description()
     ?>
 </body>
 </html>
